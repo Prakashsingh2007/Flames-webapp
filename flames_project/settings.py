5923f30f942e72ff16a0a5205785e177a0d1dd1e
Prakash Singh <prakashsinghse27@gmail.com>
2026-04-18 13:25:23 +0530

    fixed middleware ellipsis error

--- flames_project/settings.py
+++ flames_project/settings.py
@@ -53,11 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
 ]
 
 ROOT_URLCONF = 'flames_project.urls'
